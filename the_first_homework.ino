9f1305e28c3b7cbe13a29acae50d1b52e681c1b9
zl-1 <52449977+zl-1@users.noreply.github.com>
2019-07-05 17:52:23 +0800

    Update the_first_homework.ino

--- the_first_homework.ino
+++ the_first_homework.ino
@@ -1,82 +1,29 @@
-#include <Morse.h>
+#第一天 入门介绍
+ 为什么要学习开源硬件
+ 如何学习开源硬件
+ 常用软件：arduino\processing\fritzing
+ 常用网址：https://www.tinkercad.com/
+ 		  https://guides.github.com
+1、arduino:
+  Arduino是一款便捷灵活、方便上手的开源电子原型平台。包含硬件（各种型号的Arduino板）和软件（Arduino IDE)。由一个欧洲开发团队于2005年冬季开发。其成员包括Massimo Banzi、David Cuartielles、Tom Igoe、Gianluca Martino、David Mellis和Nicholas Zambetti等。
+它构建于开放原始码simple I/O介面版，并且具有使用类似Java、C语言的Processing/Wiring开发环境。主要包含两个的部分：硬件部分是可以用来做电路连接的Arduino电路板；另外一个则是Arduino IDE，你的计算机中的程序开发环境。你只要在IDE中编写程序代码，将程序上传到Arduino电路板后，程序便会告诉Arduino电路板要做些什么了。
+Arduino能通过各种各样的传感器来感知环境，通过控制灯光、马达和其他的装置来反馈、影响环境。板子上的微控制器可以通过Arduino的编程语言来编写程序，编译成二进制文件，烧录进微控制器。对Arduino的编程是通过 Arduino编程语言 (基于 Wiring)和Arduino开发环境(基于 Processing)来实现的。基于Arduino的项目，可以只包含Arduino，也可以包含Arduino和其他一些在PC上运行的软件，他们之间进行通信 (比如 Flash, Processing, MaxMSP)来实现。
+2、processing：
+  Processing是一种具有革命前瞻性的新兴计算机语言，它的概念是在电子艺术的环境下介绍程序语言，并将电子艺术的概念介绍给程序设计师。它是 Java 语言的延伸，并支持许多现有的 Java 语言架构，不过在语法 (syntax) 上简易许多，并具有许多贴心及人性化的设计。Processing 可以在 Windows、MAC OS X、MAC OS 9 、Linux 等操作系统上使用。目前最新版本为Processing 3。以 Processing 完成的作品可在个人本机端作用，或以Java Applets 的模式外输至网络上发布。
+3、fritzing:
+  Fritzing是个电子设计自动化软件。它支持设计师，艺术家，研究人员和爱好者参加从物理原型到进一步实际的产品。还支持用户记录其Arduino和其他电子为基础的原型，与他人分享，在教室里教电子，并建立一家生产印刷电路板的布局。
 
-void setup() 
-{
-  Serial.begin(9600);
 
-}
 
-void loop() 
-{
-  String comdata="";
-  int i=0;
-  int n=0;
-  while(Serial.available()>0)
-  {
-    comdata+=char(Serial.read());
-    }
-   n=comdata.length();
-    if(n>0)
-    {
-      for(i=0;i<n;i++)
-      {
-        switch(comdata[i])
-        {
-           case 'a':
-            dot();
-            dash();
-            break;
-           case 'b':
-            dash();
-            dot();
-            dot();
-            dot();
-            break;
-           case'c':
-            dash();
-            dot();
-            dash();
-            dot();
-            break;
-           case'd':
-            dash();
-            dot();
-            dot();
-            break;
-           case'e':
-            dot();
-            break;
-           case'f':
-            dot();
-            dot();
-            dash();
-            dot();
-            break;
-           case'g':
-            dash();
-            dash();
-            dot();
-            break;
-           case'h':
-            dot();
-            dot();
-            dot();
-            dot();
-            break;
-           case'i':
-            dot();
-            dot();
-            break;
-           case'j':
-            dot();
-            dash();
-            dash();
-            dash();
-            break;
-          }
-        
-        }
-      }
-  // put your main code here, to run repeatedly:
 
-}
+#第二天 arduino编程
+在arduino内完成。从电脑串口接收一串字符，通过Morse电码格式发出。
+代码如提交文件内所示。
+
+#第三天 认识电子元件及操作
+1、小车代码和电路图
+2、数码管代码及电路图
+#第四天 总结
+Morse代码tinkercad可运行代码
+markdown格式学习总结
+#end
